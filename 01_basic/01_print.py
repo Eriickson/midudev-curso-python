d844d9f39d3e2db00b6482f873fbac1ae31b9aa6
Erickson Manuel Holguín <erickson.pena@kiura.co>
2025-03-26 16:47:36 -0400

    Add basic print examples in 01_print.py

---
+++ 01_basic/01_print.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+print("Esto también funciona con una comilla")
+
+print("Python", "es", "genial")
+
+print("Python", "es", "brutal", sep="-")
+
+print("Esto se imprime", end=" ")
+print("en una linea")
